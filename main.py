bb374c82afc297e8129aff770977168500b319ba
SteinKiesel <93226473+SteinKiesel@users.noreply.github.com>
2022-06-04 17:01:46 +0200

    Hallo-Funktion hinzugefügt awdad

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
     print('hallo')
     print('neue Nachricht')
+    print('deine Mum')
     
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
